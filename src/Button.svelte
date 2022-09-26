4c865bcb448865e2f86be5f94a295b41ef4d516c
matsixfive <maffers2006@gmail.com>
2022-09-26 13:23:48 +0100

    red shaking button

--- src/Button.svelte
+++ src/Button.svelte
@@ -1,68 +1,132 @@
 <script lang="ts">
-  let clicksLeft = 10;
+	let clicksLeft = 10;
+	let shake = true;
 
-  let timeout;
+	let timeout;
 
-  const handleClick = () => {
-    if (clicksLeft > 0) {
-      clicksLeft--;
+	const handleClick = () => {
+		if (clicksLeft > 0) {
+			clicksLeft--;
 
-      timeout = setTimeout(() => {
-        clearTimeout(timeout);
-      }, 1000);
+			timeout = setTimeout(() => {
+				clearTimeout(timeout);
+			}, 100);
 
-      // send click to server...
-    }
-  };
+			// send click to server...
+		}
+	};
 </script>
 
-<button class:timeout={clicksLeft === 0} on:click={handleClick}>
-  <span>
-    {clicksLeft.toLocaleString()}
-  </span>
+<button
+	class:timeout={clicksLeft === 0}
+	on:click={handleClick}
+	class:shake
+	on:mousedown={() => {
+		shake = false;
+		setTimeout(() => {
+			shake = true;
+		}, 1);
+	}}
+>
+	<span>
+		{clicksLeft.toLocaleString()}
+	</span>
 </button>
 
 {#if clicksLeft === 0}
-  <p>no more clicks left for today</p>
+	<p>no more clicks left for today</p>
 {/if}
 
 <style lang="scss">
-  .timeout {
-    color: hsl(35, 34%, 45%);
-  }
+	$rad: 7px;
+	$blur: 14px;
 
-  button {
-    width: calc(min(400px, 97%) - 0.5rem);
-    margin: 1rem;
-    aspect-ratio: 1;
-    border: none;
-    cursor: pointer;
-    padding: 0;
+	@mixin tan-button {
+		color: black;
+		border-radius: 15%;
+		background: #e8dfca;
+		box-shadow: $rad $rad $blur #ccc4b2,
+			calc(-1 * $rad) calc(-1 * $rad) $blur #fffae2,
+			inset $rad $rad $blur transparent,
+			inset calc(-1 * $rad) calc(-1 * $rad) $blur transparent;
 
-    font-family: "ubuntu";
-    font-size: 5em;
-    line-height: 1em;
-    overflow-wrap: break-word;
+		&:active {
+			color: black;
+			border-radius: 15%;
+			background: #e8dfca;
+			box-shadow: $rad $rad $blur transparent,
+				calc(-1 * $rad) calc(-1 * $rad) $blur transparent,
+				inset $rad $rad $blur #ccc4b2,
+				inset calc(-1 * $rad) calc(-1 * $rad) $blur #fffae2;
+		}
+	}
 
-    $rad: 7px;
-    $blur: 14px;
+	@mixin red-button {
+		color: white !important;
+		border-radius: 15%;
+		background: #da2f2f;
+		box-shadow: $rad $rad $blur #ccc4b2,
+			calc(-1 * $rad) calc(-1 * $rad) $blur #fffae2,
+			inset $rad $rad $blur transparent,
+			inset calc(-1 * $rad) calc(-1 * $rad) $blur transparent;
 
-    border-radius: 15%;
-    background: #e8dfca;
-    box-shadow: $rad $rad $blur #ccc4b2,
-      calc(-1 * $rad) calc(-1 * $rad) $blur #fffae2,
-      inset $rad $rad $blur transparent,
-      inset calc(-1 * $rad) calc(-1 * $rad) $blur transparent;
+		/* &:active {
+			color: white !important;
+			border-radius: 15%;
+			background: #da2f2f;
+			box-shadow: $rad $rad $blur transparent,
+				calc(-1 * $rad) calc(-1 * $rad) $blur transparent,
+				inset $rad $rad $blur #c02929,
+				inset calc(-1 * $rad) calc(-1 * $rad) $blur #f43535;
+		} */
+	}
 
-    transition: box-shadow 250ms cubic-bezier(0, 0.7, 0, 1);
+	/* .timeout {
+		// color: hsl(35, 34%, 45%);
+	} */
 
-    &:active {
-      border-radius: 61px;
-      background: #e8dfca;
-      box-shadow: $rad $rad $blur transparent,
-        calc(-1 * $rad) calc(-1 * $rad) $blur transparent,
-        inset $rad $rad $blur #ccc4b2,
-        inset calc(-1 * $rad) calc(-1 * $rad) $blur #fffae2;
-    }
-  }
+	.timeout.shake {
+		@keyframes shake {
+			0% {
+				transform: translateX(0) rotate(0);
+			}
+
+			25% {
+				transform: translateX(10px) rotate(1deg);
+				@include red-button();
+			}
+
+			50% {
+				transform: translateX(-10px) rotate(-1deg);
+			}
+
+			75% {
+				transform: translateX(10px) rotate(1deg);
+			}
+
+			100% {
+				transform: translateX(0) rotate(0);
+			}
+		}
+
+		animation: shake 200ms;
+	}
+
+	button {
+		width: calc(min(400px, 97%) - 0.5rem);
+		margin: 1rem;
+		aspect-ratio: 1;
+		border: none;
+		cursor: pointer;
+		padding: 0;
+
+		font-family: "ubuntu";
+		font-size: 5em;
+		line-height: 1em;
+		overflow-wrap: break-word;
+
+		@include tan-button();
+
+		transition: box-shadow 0ms cubic-bezier(0, 0.7, 0, 1);
+	}
 </style>
