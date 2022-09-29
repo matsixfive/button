9a4a56310435eb2a958c31a1a309fd84c07e56fc
matsixfive <maffers2006@gmail.com>
2022-09-29 01:31:41 +0100

    wicpunter and confirmation

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,35 @@
 <script lang="ts">
-  import Button from "./Button.svelte";
+	import Button from "./Button.svelte";
+	import WinCounter from "./WinCounter.svelte";
+
+	let flash = false;
 </script>
 
-<main>
-  <Button />
+<main class:flash>
+	<div class="win-container">
+		<WinCounter />
+	</div>
+
+	<Button bind:flash />
 </main>
 
-<style>
-  main {
-    text-align: center;
-    /* padding: 1em; */
-    /* max-width: 240px; */
-    min-height: 100vh;
-    min-height: fill-avaliable;
-    min-height: --webkit-fill-avaliable;
-    margin: 0 auto;
-    background: #e8dfca;
-  }
+<style lang="scss">
+	main {
+		text-align: center;
+		min-height: 100vh;
+		margin: 0 auto;
+		background: #e8dfca;
 
-  /* @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  } */
+		&.flash {
+			background: #faf2e1;
+		}
+	}
+
+
+	.win-container {
+		position: absolute;
+		top: 1em;
+		right: 1em;
+		z-index: 1;
+	}
 </style>
---
+++ src/WinCounter.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { wins } from "./stores/wins";
+</script>
+
+<div class="inline">
+	<div class="circle">
+		<span class="win-count">{$wins}</span>
+	</div>
+</div>
+
+<style lang="scss">
+	.circle {
+		border-radius: max(100vh, 100vw);
+		background-color: #e82c0e;
+		display: flex;
+		min-width: calc(2em - 12px);
+		min-height: 2em;
+		align-items: center;
+		justify-content: center;
+		padding: 0 6px 0 6px;
+	}
+	
+	.win-count {
+		color: white;
+		font-family: "ubuntu";
+		font-weight: bold;
+	}
+	
+	.inline {
+		display: inline-block;
+	}
+</style>
